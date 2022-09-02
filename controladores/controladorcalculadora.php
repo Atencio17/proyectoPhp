d8c91d0e1b471ee7cc6d1c37993b3e3ac5ed1756
Atencio17 <atenciomuniveandresfelipe2@gmail.com>
2022-09-02 08:47:36 -0500

    poo en ejercicio calculadora

--- controladores/controladorcalculadora.php
+++ controladores/controladorcalculadora.php
@@ -1,23 +1,35 @@
 <?php
 class ControladorCalculadora {
 
-    public function sumar($operadorUno,$operadorDos){
-        
+    private $operadorUno;
+    private $operadorDos;
+
+    function __construct($operadorUno="",$operadorDos=""){
+        $this->$operadorUno=$operadorUno;
+        $this->$operadorDos=$operadorDos;
+    }
+
+    public function sumar($operadorUno="",$operadorDos=""){
+        $operadorUno = empty($operadorUno) ? $this->$operadorUno : $operadorUno;
+        $operadorDos = empty($operadorDos) ? $this->$operadorDos : $operadorDos;
         return $operadorUno + $operadorDos;
     }
 
-    public function restar($operadorUno,$operadorDos){
-        
+    public function restar($operadorUno="",$operadorDos=""){
+        $operadorUno = empty($operadorUno) ? $this->$operadorUno : $operadorUno;
+        $operadorDos = empty($operadorDos) ? $this->$operadorDos : $operadorDos;
         return $operadorUno - $operadorDos;
     }
 
-    public function dividir($operadorUno,$operadorDos){
-        
+    public function dividir($operadorUno="",$operadorDos=""){
+        $operadorUno = empty($operadorUno) ? $this->$operadorUno : $operadorUno;
+        $operadorDos = empty($operadorDos) ? $this->$operadorDos : $operadorDos;
         return $operadorUno / $operadorDos;
     }
     
-    public function multiplicar($operadorUno,$operadorDos){
-        
+    public function multiplicar($operadorUno="",$operadorDos=""){
+        $operadorUno = empty($operadorUno) ? $this->$operadorUno : $operadorUno;
+        $operadorDos = empty($operadorDos) ? $this->$operadorDos : $operadorDos;
         return $operadorUno * $operadorDos;
     }
 }
--- vistas/vistacalculadora.php
+++ vistas/vistacalculadora.php
@@ -2,5 +2,23 @@
 include "..\controladores\controladorcalculadora.php";
 
 $controladorcalculadora = new ControladorCalculadora();
-
-echo $controladorcalculadora->sumar(34,56);
+$operadorUno = $_POST['operadorUno'];
+$operadorDos = $_POST['operadorDos'];
+$operacion = $_POST['operacion'];
+switch ($operacion) {
+    case '+':
+    echo $controladorcalculadora->sumar($operadorUno,$operadorDos);
+        break;
+    case '-':
+    echo $controladorcalculadora->restar($operadorUno,$operadorDos);
+        break;
+    case '*':
+    echo $controladorcalculadora->multiplicar($operadorUno,$operadorDos);
+        break;
+    case '/':
+    echo $controladorcalculadora->dividir($operadorUno,$operadorDos);
+        break;
+    default:
+    echo "Error";
+    break;
+}
